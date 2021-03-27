3909f5385b51af16161ed1621ca48ab94c2c2d4f
pavankpdev <pavankumarkpkp@gmail.com>
2021-03-27 09:38:58 +0530

    add express

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import { MikroORM } from "@mikro-orm/core";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
 
 // Configs
 import microConfig from "./mikro-orm.config";
@@ -13,6 +15,13 @@ const main = async () => {
   // Db setup
   const orm = await MikroORM.init(microConfig);
   await orm.getMigrator().up();
+
+  // Express setup
+  const app = express();
+
+  app.get("/", (_, res) => res.json({ YO: "YO" }));
+
+  app.listen(4000, () => console.log("Listening on port 4000"));
 };
 
 main();
